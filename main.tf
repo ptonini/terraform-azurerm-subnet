e95fe5b79d2a26256f8b7064d44408d09d3687ed
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-11-23 21:55:50 -0300

    chore: first commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "azurerm_subnet" "this" {
+  name                 = var.name
+  resource_group_name  = var.rg.name
+  virtual_network_name = var.vnet.name
+  address_prefixes     = var.address_prefixes
+  service_endpoints    = var.service_endpoints
+  dynamic "delegation" {
+    for_each = var.delegation == null ? [] : [var.delegation]
+    content {
+      name = delegation.value["name"]
+      service_delegation {
+        name = delegation.value["service_delegation"]["name"]
+        actions = delegation.value["service_delegation"]["actions"]
+      }
+    }
+  }
+}
+
+resource "azurerm_subnet_nat_gateway_association" "this" {
+  count          = var.nat_gateway == null ? 0 : 1
+  subnet_id      = azurerm_subnet.this.id
+  nat_gateway_id = var.nat_gateway.id
+}
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "name" {}
+
+variable "rg" {
+  type = object({name = string})
+}
+
+variable "vnet" {
+  type = object({name = string})
+}
+variable "address_prefixes" {
+  type = list(string)
+}
+
+variable "nat_gateway" {
+  default = null
+}
+
+variable "service_endpoints" {
+  default = []
+}
+
+variable "delegation" {
+  default = null
+  type = object({
+    name = string
+    service_delegation = object({
+      name = string
+      actions = list(string)
+    })
+  })
+}
